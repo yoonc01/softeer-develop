7cdbfab8c482082c8279e0eba8bffd59d357aa22
yoonc01 <yoonc01@hotmail.com>
2025-04-06 18:38:33 +0900

    chore: Add tailwindcss on admin

--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -1,12 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@ui': path.resolve(__dirname, '../../packages/ui/src'),
-    },
-  },
+  plugins: [react(), tailwindcss()],
 });
