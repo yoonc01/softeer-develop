eda25e3b265cace3500f43788fb510a5e479aa8e
yoonc01 <yoonc01@hotmail.com>
2025-04-07 15:42:34 +0900

    chore: Alias src to @ and set jest, ts to recognize @ as src

--- apps/service/jest.config.cjs
+++ apps/service/jest.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
   testEnvironment: "jsdom",
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   moduleNameMapper: {
-    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+    "^@/(.*)$": "<rootDir>/src/$1",
   },
   transform: {
     "^.+\\.(ts|tsx)$": "ts-jest",
