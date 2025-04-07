6282ecadf3ebae234585c2b574e7c4db4d092aec
yoonc01 <yoonc01@hotmail.com>
2025-04-07 15:38:55 +0900

    feat: Remove test and hello component that do not need

--- apps/service/src/components/Hello.test.tsx
+++
@@ -1,7 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import { Hello } from "./Hello";
-
-test("Hello 컴포넌트는 이름을 출력한다", () => {
-  render(<Hello name="world" />);
-  expect(screen.getByText("Hello, world!")).toBeInTheDocument();
-});
